29fb76d888077a4d3c11accbc9c4208c81facbe7
Felipe Lessa <felipe.lessa@gmail.com>
2011-04-18 16:15:09 +0400

    Make deriveSafeCopy work for data types with multiple constructors.
    Ignore-this: 53a7c134775bddecc97452089d8b4161
    
    darcs-hash:20110418121509-0b5ec-4b87cdf2d0db63a7ae23c6c5e483f0085849395c

--- src/Data/SafeCopy/Derive.hs
+++ src/Data/SafeCopy/Derive.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Data.SafeCopy.Derive where
 
+import Data.Binary (Get, getWord8, putWord8)
 import Data.SafeCopy.SafeCopy
 
 import Language.Haskell.TH hiding (Kind(..))
@@ -18,37 +19,58 @@ deriveSafeCopy :: Integer -> Name -> Name -> Q [Dec]
 deriveSafeCopy versionId kindName tyName
     = do info <- reify tyName
          case info of
-           TyConI (DataD cxt _name tyvars [con] _derivs)
-             -> worker cxt tyvars con
-           TyConI DataD{}
-             -> fail $ "Can't derive SafeCopy instance for: " ++ show tyName ++
-                       ". The datatype must have exactly one constructor."
+           TyConI (DataD cxt _name tyvars cons _derivs)
+             | length cons > 255 -> fail $ "Can't derive SafeCopy instance for: " ++ show tyName ++
+                                           ". The datatype must have less than 256 constructors."
+             | otherwise         -> worker cxt tyvars (zip [0..] cons)
            TyConI (NewtypeD cxt _name tyvars con _derivs)
-             -> worker cxt tyvars con
+             -> worker cxt tyvars [(0, con)]
            _ -> fail $ "Can't derive SafeCopy instance for: " ++ show (tyName, info)
-    where worker context tyvars con
-              = do putCopyDec <- mkPutCopy con
-                   getCopyDec <- mkGetCopy con
-                   let ty = foldl appT (conT tyName) [ varT var | PlainTV var <- tyvars ]
-                   i <- instanceD (cxt $ [classP ''SafeCopy [varT var] | PlainTV var <- tyvars] ++ map return context)
-                                  (conT ''SafeCopy `appT` ty)
-                                  [ putCopyDec
-                                  , getCopyDec
-                                  , valD (varP 'version) (normalB (litE (integerL versionId))) []
-                                  , valD (varP 'kind) (normalB (varE kindName)) []
-                                  ]
-                   return [i]
-          mkPutCopy con
-              = do putVars <- replicateM (conSize con) (newName "arg")
-                   let putClause   = conP (conName con) (map varP putVars)
-                       putCopyBody = varE 'contain `appE` doE ( [ noBindS $ varE 'safePut `appE` varE var | var <- putVars ] ++
-                                                                [ noBindS $ varE 'return `appE` tupE [] ])
-                   return $ funD 'putCopy [clause [putClause] (normalB putCopyBody) []]
-          mkGetCopy con
-              = do let getBase     = appE (varE 'return) (conE (conName con))
-                       getArgs     = foldl (\a b -> infixE (Just a) b (Just (varE 'safeGet))) getBase (replicate (conSize con) (varE '(<*>)))
-                       getCopyBody = varE 'contain `appE` getArgs
-                   return $ valD (varP 'getCopy) (normalB getCopyBody) []
+    where worker context tyvars cons
+              = let ty = foldl appT (conT tyName) [ varT var | PlainTV var <- tyvars ]
+                in (:[]) <$> instanceD (cxt $ [classP ''SafeCopy [varT var] | PlainTV var <- tyvars] ++ map return context)
+                                       (conT ''SafeCopy `appT` ty)
+                                       [ mkPutCopy cons
+                                       , mkGetCopy cons
+                                       , valD (varP 'version) (normalB (litE (integerL versionId))) []
+                                       , valD (varP 'kind) (normalB (varE kindName)) []
+                                       ]
+          mkPutCopy cons = funD 'putCopy $ map mkPutClause cons
+              where
+                manyConstructors = length cons > 1
+                mkPutClause (conNumber, con)
+                    = do putVars <- replicateM (conSize con) (newName "arg")
+                         let putClause   = conP (conName con) (map varP putVars)
+                             putCopyBody = varE 'contain `appE` doE (
+                                             [ noBindS $ varE 'putWord8 `appE` (litE $ IntegerL conNumber) | manyConstructors ] ++
+                                             [ noBindS $ varE 'safePut `appE` varE var | var <- putVars ] ++
+                                             [ noBindS $ varE 'return `appE` tupE [] ])
+                         clause [putClause] (normalB putCopyBody) []
+          mkGetCopy cons = valD (varP 'getCopy) (normalB $ varE 'contain `appE` getCopyBody) []
+              where
+                getCopyBody
+                    = case cons of
+                        [(_, con)] -> mkGetBody con
+                        _          -> do
+                          tagVar <- newName "tag"
+                          doE [ bindS (varP tagVar) (varE 'getWord8)
+                              , noBindS $ caseE (varE tagVar) (
+                                  [ match (litP $ IntegerL i) (normalB $ mkGetBody con) [] | (i, con) <- cons ] ++
+                                  [ match wildP (normalB $ varE 'fail `appE` errorMsg tagVar) [] ]) ]
+                mkGetBody con
+                    = let getBase = appE (varE 'return) (conE (conName con))
+                      in foldl (\a b -> infixE (Just a) b (Just (varE 'safeGet))) getBase (replicate (conSize con) (varE '(<*>)))
+                errorMsg tagVar = infixE (Just $ strE str1) (varE '(++)) $ Just $
+                                  infixE (Just tagStr) (varE '(++)) (Just $ strE str2)
+                    where
+                      strE = litE . StringL
+                      tagStr = varE 'show `appE` varE tagVar
+                      str1 = "Could not identify tag \""
+                      str2 = concat [ "\" for type "
+                                    , show tyName
+                                    , " that has only "
+                                    , show (length cons)
+                                    , " constructors.  Maybe your data is corrupted?" ]
           conSize (NormalC _name args) = length args
           conSize (RecC _name recs) = length recs
           conSize InfixC{} = 2
