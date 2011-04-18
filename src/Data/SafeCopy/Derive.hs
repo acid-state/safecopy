289b9cadb1a51c61cb1f0eaa0a4082f75d00c16d
Felipe Lessa <felipe.lessa@gmail.com>
2011-04-18 16:24:44 +0400

    Separate many of the deriveSafeCopy's internal functions into the top-level.
    Ignore-this: 1fc9e42a8351845b42a1235fa0a01948
    
    darcs-hash:20110418122444-0b5ec-ca48592f26ca5c04e6ea80d50ad9d5f075f6641c

--- src/Data/SafeCopy/Derive.hs
+++ src/Data/SafeCopy/Derive.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
-module Data.SafeCopy.Derive where
+module Data.SafeCopy.Derive (deriveSafeCopy) where
 
 import Data.Binary (Get, getWord8, putWord8)
 import Data.SafeCopy.SafeCopy
@@ -31,52 +31,60 @@ deriveSafeCopy versionId kindName tyName
                 in (:[]) <$> instanceD (cxt $ [classP ''SafeCopy [varT var] | PlainTV var <- tyvars] ++ map return context)
                                        (conT ''SafeCopy `appT` ty)
                                        [ mkPutCopy cons
-                                       , mkGetCopy cons
+                                       , mkGetCopy tyName cons
                                        , valD (varP 'version) (normalB (litE (integerL versionId))) []
                                        , valD (varP 'kind) (normalB (varE kindName)) []
                                        ]
-          mkPutCopy cons = funD 'putCopy $ map mkPutClause cons
-              where
-                manyConstructors = length cons > 1
-                mkPutClause (conNumber, con)
-                    = do putVars <- replicateM (conSize con) (newName "arg")
-                         let putClause   = conP (conName con) (map varP putVars)
-                             putCopyBody = varE 'contain `appE` doE (
-                                             [ noBindS $ varE 'putWord8 `appE` (litE $ IntegerL conNumber) | manyConstructors ] ++
-                                             [ noBindS $ varE 'safePut `appE` varE var | var <- putVars ] ++
-                                             [ noBindS $ varE 'return `appE` tupE [] ])
-                         clause [putClause] (normalB putCopyBody) []
-          mkGetCopy cons = valD (varP 'getCopy) (normalB $ varE 'contain `appE` getCopyBody) []
-              where
-                getCopyBody
-                    = case cons of
-                        [(_, con)] -> mkGetBody con
-                        _          -> do
-                          tagVar <- newName "tag"
-                          doE [ bindS (varP tagVar) (varE 'getWord8)
-                              , noBindS $ caseE (varE tagVar) (
-                                  [ match (litP $ IntegerL i) (normalB $ mkGetBody con) [] | (i, con) <- cons ] ++
-                                  [ match wildP (normalB $ varE 'fail `appE` errorMsg tagVar) [] ]) ]
-                mkGetBody con
-                    = let getBase = appE (varE 'return) (conE (conName con))
-                      in foldl (\a b -> infixE (Just a) b (Just (varE 'safeGet))) getBase (replicate (conSize con) (varE '(<*>)))
-                errorMsg tagVar = infixE (Just $ strE str1) (varE '(++)) $ Just $
-                                  infixE (Just tagStr) (varE '(++)) (Just $ strE str2)
-                    where
-                      strE = litE . StringL
-                      tagStr = varE 'show `appE` varE tagVar
-                      str1 = "Could not identify tag \""
-                      str2 = concat [ "\" for type "
-                                    , show tyName
-                                    , " that has only "
-                                    , show (length cons)
-                                    , " constructors.  Maybe your data is corrupted?" ]
-          conSize (NormalC _name args) = length args
-          conSize (RecC _name recs) = length recs
-          conSize InfixC{} = 2
-          conSize ForallC{} = error "Found complex constructor. Cannot derive SafeCopy for it."
-          conName (NormalC name _args) = name
-          conName (RecC name _recs) = name
-          conName (InfixC _ name _) = name
+
+mkPutCopy :: [(Integer, Con)] -> DecQ
+mkPutCopy cons = funD 'putCopy $ map mkPutClause cons
+    where
+      manyConstructors = length cons > 1
+      mkPutClause (conNumber, con)
+          = do putVars <- replicateM (conSize con) (newName "arg")
+               let putClause   = conP (conName con) (map varP putVars)
+                   putCopyBody = varE 'contain `appE` doE (
+                                   [ noBindS $ varE 'putWord8 `appE` (litE $ IntegerL conNumber) | manyConstructors ] ++
+                                   [ noBindS $ varE 'safePut `appE` varE var | var <- putVars ] ++
+                                   [ noBindS $ varE 'return `appE` tupE [] ])
+               clause [putClause] (normalB putCopyBody) []
+
+mkGetCopy :: Name -> [(Integer, Con)] -> DecQ
+mkGetCopy tyName cons = valD (varP 'getCopy) (normalB $ varE 'contain `appE` getCopyBody) []
+    where
+      getCopyBody
+          = case cons of
+              [(_, con)] -> mkGetBody con
+              _          -> do
+                tagVar <- newName "tag"
+                doE [ bindS (varP tagVar) (varE 'getWord8)
+                    , noBindS $ caseE (varE tagVar) (
+                        [ match (litP $ IntegerL i) (normalB $ mkGetBody con) [] | (i, con) <- cons ] ++
+                        [ match wildP (normalB $ varE 'fail `appE` errorMsg tagVar) [] ]) ]
+      mkGetBody con
+          = let getBase = appE (varE 'return) (conE (conName con))
+            in foldl (\a b -> infixE (Just a) b (Just (varE 'safeGet))) getBase (replicate (conSize con) (varE '(<*>)))
+      errorMsg tagVar = infixE (Just $ strE str1) (varE '(++)) $ Just $
+                        infixE (Just tagStr) (varE '(++)) (Just $ strE str2)
+          where
+            strE = litE . StringL
+            tagStr = varE 'show `appE` varE tagVar
+            str1 = "Could not identify tag \""
+            str2 = concat [ "\" for type "
+                          , show tyName
+                          , " that has only "
+                          , show (length cons)
+                          , " constructors.  Maybe your data is corrupted?" ]
+
+conSize :: Con -> Int
+conSize (NormalC _name args) = length args
+conSize (RecC _name recs)    = length recs
+conSize InfixC{}             = 2
+conSize ForallC{}            = error "Found complex constructor. Cannot derive SafeCopy for it."
+
+conName :: Con -> Name
+conName (NormalC name _args) = name
+conName (RecC name _recs)    = name
+conName (InfixC _ name _)    = name
 
 
